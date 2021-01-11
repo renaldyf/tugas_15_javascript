01d23954646e569571445c29a302afff950abc8d
renaldy-repo <renaldy.frakasya@gmail.com>
2021-01-11 17:38:10 +0700

    Tugas 15 JavaScript
    Tugas 15 JS an Renaldy Frakasya R.

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+function tugas15() {
+  var x = 8;
+  var y = 6;
+  console.log("Nilai AB :",x);
+  console.log("Nilai BC :",y);
+
+  var z = Math.pow(x,2) + Math.pow(y,2);
+  console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah :", Math.sqrt(z), "cm");
+}
+
+tugas15()
